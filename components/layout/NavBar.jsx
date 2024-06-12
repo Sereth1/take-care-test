752480ba5850866e7331a1059d6368864d78fb69
Sereth1 <n1ghtw0olf@outlook.com>
2024-06-12 19:20:58 +0800

    wtf

--- components/layout/NavBar.jsx
+++ components/layout/NavBar.jsx
@@ -6,58 +6,63 @@ const Navbar = () => {
   return (
     <div className="bg-white rounded-full shadow-md px-6 py-4 flex items-center justify-between mx-4 my-4 font-bold">
       <div className="flex items-center">
-        <span className="text-xl font-bold container">
+        <span className="text-xl font-bold container relative w-[211px] h-[48px]">
           <Image
             src="/images/navBar/TestLogo.png"
             alt="TechCare Logo"
-            width={211}
-            height={48}
-            className="mr-4"
+            layout="fill"
+            objectFit="contain"
           />
         </span>
       </div>
       <div className="flex items-center space-x-8">
         {navItems.map((item, i) => (
-          <Link key={i} href={item.href}>
-            <div className="flex items-center text-gray-600 hover:bg-navBarHover hover:rounded-full p-2 box-border">
-              <Image
-                src={item.src}
-                alt={item.alt}
-                width={30}
-                height={30}
-                className="mr-2"
-              />
+          <Link key={i} href={item.href} passHref>
+            <div className="flex items-center text-gray-600 hover:bg-navBarHover hover:rounded-full p-2 box-border cursor-pointer">
+              <div className="relative w-[30px] h-[30px] mr-2">
+                <Image
+                  src={item.src}
+                  alt={item.alt}
+                  layout="fill"
+                  objectFit="contain"
+                />
+              </div>
               {item.label}
             </div>
           </Link>
         ))}
       </div>
       <div className="flex items-center">
-        <Image
-          src="/images/navBar/senior.png"
-          alt="Dr. Jose Simmons"
-          width={40}
-          height={40}
-          className="rounded-full mr-4"
-        />
+        <div className="relative w-[40px] h-[40px] mr-4">
+          <Image
+            src="/images/navBar/senior.png"
+            alt="Dr. Jose Simmons"
+            layout="fill"
+            objectFit="cover"
+            className="rounded-full"
+          />
+        </div>
         <div className="text-gray-600">
           <p>Dr. Jose Simmons</p>
           <p className="text-sm">General Practitioner</p>
         </div>
         <div className="border border-black-600 mx-3 h-10"></div>
-        <Image
-          src="/icons/navBar/gear.svg"
-          alt="Settings"
-          width={19}
-          height={20}
-          className="rounded-full mr-4"
-        />
-        <Image
-          src="/icons/navBar/dots.svg"
-          alt="More options"
-          width={1}
-          height={20}
-        />
+        <div className="relative w-[19px] h-[20px] mr-4">
+          <Image
+            src="/icons/navBar/gear.svg"
+            alt="Settings"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="relative w-[4px] h-[20px]">
+          <Image
+            src="/icons/navBar/dots.svg"
+            alt="More options"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
       </div>
     </div>
   );
